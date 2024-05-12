18a20095771900a26623ae934f5352296742d72f
Subash-07 <157941230+Subash-07@users.noreply.github.com>
2024-05-12 17:51:44 +0530

    Day 4 Task

---
+++ Task Day 3/Day 4.js
@@ -0,0 +1,49 @@
+
+ resume;
+    {
+      "personal_information" {
+        "name": "Nithya subash D",
+        "address": "22/7 Bajanai koli st,west Tambaram ch 45",
+        "phone": "9840346194",
+        "email": "nithyasubash0711@gmail.com",
+     
+      }
+      "objective": "To secure a challenging position in a reputable organization to expand my learnings, knowledge, and skills.",
+      "work_experience" [
+        {
+          "job_title": "senior exective",
+          "company": "Hapag lloyd Globa service",
+          "location": "chennai",
+          "start_date": "Feb 2017",
+          "end_date": "Present",
+          "responsibilities": [
+            "Developed and maintained code for in-house and client websites primarily using HTML, CSS, Sass, JavaScript, and jQuery",
+            "Managed a team of four developers, overseeing projects from conception to delivery",
+            "Ensured functionality and efficiency of applications and websites"
+          ]
+        },
+        {
+          "job_title": "Dataspaclist",
+          "company": "HTC Global service.",
+          "location": "chennai",
+          "start_date": "May 2015"
+          "end date": "Jan 2017",
+          "responsibilities": [
+            "Designed, implemented, and monitored web pages and sites for continuous improvement",
+            "Tested websites across browsers, operating systems and devices",
+            "Maintained, updated, and optimized server and database operations"
+          ]
+        }
+      ],
+      "education": [
+        {
+          "degree": "Bachelor of Bussineess Administation",
+          "school": "University of Madara",
+          "location": "Chennai",
+          "graduation_year": 2012
+        }
+      ],
+      "skills": [
+        "HTML", "CSS", "JavaScript", "React",
+      ]
+  
