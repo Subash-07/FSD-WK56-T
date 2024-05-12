f03adf0c77c1b6989c769e3c619e73cbd2861065
Subash-07 <157941230+Subash-07@users.noreply.github.com>
2024-05-12 18:04:08 +0530

    Day4task2

---
+++ Task Day 3/Day4task2.js
@@ -0,0 +1,9 @@
+screen
+The screen object provides information about the physical screen's display properties.
+
+Document
+The Document object represent the DOM (Document Object Model) of the web page loaded in the window.
+
+window
+The Window object represents an open window in a browser and serves as the globla object for the JS code running in the browser window..DOM
+
