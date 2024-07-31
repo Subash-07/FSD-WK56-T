9b55dad9c52fbcfe122be47a164c7c9604694541
Subash-07 <157941230+Subash-07@users.noreply.github.com>
2024-07-31 09:55:51 +0530

    new changes

---
+++ npm -project/Artvista Gallery project/src/pages/productlisting.jsx
@@ -0,0 +1,10 @@
+const ProductListing = () => {
+  return (
+    <div>
+      <h1>Product Listing</h1>
+      {/* Your product listing code here */}
+    </div>
+  );
+};
+
+export default ProductListing;
